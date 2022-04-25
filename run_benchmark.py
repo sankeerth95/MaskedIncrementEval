d608d5e0dfc60a2be0819cbf34d493e6e4343a71
sankeerth <sankeerth95@gmail.com>
2022-04-24 23:22:12 -0400

    modified todos, network shape conv

--- run_benchmark.py
+++ run_benchmark.py
@@ -17,19 +17,55 @@ def run_sequence_of_deltaconv_sparse_benchmarks():
         print(ret)
 
 
-def compare_deltaconv_with_torchconv_(ksize=3):
+
+
+
+
+
+
+def compare_conv_with_network_shapes(compare_benchmarks):
+
+
+    print(compare_benchmarks(in_shape=(1, 5, 264, 352),     shape=(5, 32),      k=5))
+
+    print(compare_benchmarks(in_shape=(1, 32, 264, 352),    shape=(32, 64),     k=5))
+    print(compare_benchmarks(in_shape=(1, 128, 132, 176),   shape=(128, 256),   k=3)) 
+
+
+    print(compare_benchmarks(in_shape=(1, 64, 132, 176),    shape=(64, 128),    k=5)) 
+    print(compare_benchmarks(in_shape=(1, 256, 66, 88),     shape=(256, 512),   k=3)) 
+
+    print(compare_benchmarks(in_shape=(1, 128, 66, 88),     shape=(128, 256),   k=5)) 
+    print(compare_benchmarks(in_shape=(1, 512, 33, 44),     shape=(512, 1024),  k=3)) 
+
+
+    print(compare_benchmarks(in_shape=(1, 256, 33, 44),     shape=(256, 256),   k=3)) 
+    print(compare_benchmarks(in_shape=(1, 256, 33, 44),     shape=(256, 256),   k=3)) 
+    print(compare_benchmarks(in_shape=(1, 256, 33, 44),     shape=(256, 256),   k=3)) 
+    print(compare_benchmarks(in_shape=(1, 256, 33, 44),     shape=(256, 256),   k=3)) 
+
+
+    print(compare_benchmarks(in_shape=(1, 256, 66, 88),     shape=(256, 128),   k=5)) 
+
+    print(compare_benchmarks(in_shape=(1, 128, 132, 176),   shape=(128, 64) ,   k=5)) 
+
+    print(compare_benchmarks(in_shape=(1, 64, 264, 352),    shape=(64, 32)  ,   k=5)) 
+
+    print(compare_benchmarks(in_shape=(1, 32, 264, 352),    shape=(32, 1)   ,   k=1)) 
+
+
+
+
+
+def compare_deltaconv_with_torchconv_():
     
     def compare_benchmarks(in_shape, shape=(32, 64), k=3, stride=1):
         t1 = benchmark_conv(in_shape=in_shape, shape=shape, k=k)
         t2 = benchmark_deltaconv(in_shape=in_shape, shape=shape, k=k)
         return t1/t2
 
-    print(compare_benchmarks(in_shape=(1, 32, 346, 260), shape=(32, 64), k=ksize))
-    print(compare_benchmarks(in_shape=(1, 3, 346, 260), shape=(3, 32), k=ksize))
-    print(compare_benchmarks(in_shape=(1, 32*2, 346, 260), shape=(32*2, 64*2), k=ksize)) # fastest as of now
-    print(compare_benchmarks(in_shape=(1, 32*4, 346, 260), shape=(32*4, 64*4), k=ksize))
-    print(compare_benchmarks(in_shape=(1, 32, 346//2, 260//2), shape=(32, 64), k=ksize))
-    print(compare_benchmarks(in_shape=(1, 32, 346//4, 260//4), shape=(32, 64), k=ksize))
+    compare_conv_with_network_shapes(compare_benchmarks)
+
 
 
 
