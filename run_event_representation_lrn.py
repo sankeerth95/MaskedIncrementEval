7b332d14c3ccbcdfefe3d3836400f5ad74519e35
sankeerth <sankeerth95@gmail.com>
2022-05-09 22:08:35 -0400

    benchmarking resnet arch

--- run_event_representation_lrn.py
+++ run_event_representation_lrn.py
@@ -1,53 +1,42 @@
 import argparse, os
 import torch
+from torch.utils.data import DataLoader
 from tqdm import tqdm
+from data_fetchers.continuous_event_datasets import ContinuousEventsDataset
+from ev_projs.rpg_async.dataloader.dataset import NCaltech101_ObjectDetection
 from ev_projs.rpg_event_representation_learning.utils.dataset import NCaltech101
 from ev_projs.rpg_event_representation_learning.utils.loader import Loader
 from incr_modules.event_representation_incr import ClassifierIncrEval
 from ev_projs.rpg_event_representation_learning.utils.loss import cross_entropy_loss_and_accuracy
-
-
-def FLAGS():
-    parser = argparse.ArgumentParser(
-        """Deep Learning for Events. Supply a config file.""")
-
-    # can be set in config
-    parser.add_argument("--checkpoint", default="", required=False)
-    parser.add_argument("--test_dataset", default="", required=False)
-    parser.add_argument("--device", default="cuda:0")
-    parser.add_argument("--num_workers", type=int, default=4)
-    parser.add_argument("--batch_size", type=int, default=4)
-    parser.add_argument("--pin_memory", type=bool, default=True)
-
-    flags = parser.parse_args()
-
-    # assert os.path.isdir(os.path.dirname(flags.checkpoint)), f"Checkpoint{flags.checkpoint} not found."
-    # assert os.path.isdir(flags.test_dataset), f"Test dataset directory {flags.test_dataset} not found."
-
-    # print(f"----------------------------\n"
-    #       f"Starting testing with \n"
-    #       f"checkpoint: {flags.checkpoint}\n"
-    #       f"test_dataset: {flags.test_dataset}\n"
-    #       f"batch_size: {flags.batch_size}\n"
-    #       f"device: {flags.device}\n"
-    #       f"----------------------------")
-
-    return flags
+from torch.profiler import profile, record_function, ProfilerActivity
 
 
 
 if __name__ == '__main__':
     device='cuda'
-    # memory_format=torch.channels_last
-    # base_folder = '/home/sankeerth/ev/depth_proj/data/test/'
 
-    # dataset = ContinuousEventsDataset(base_folder=base_folder, event_folder='events/data',\
-    #         width=346, height=260, window_size = 0.05, time_shift = 0.05) # 1 ms time shift
+    device='cuda'
+    height, width = 180, 240
+    continuous_dataset = False
+    if continuous_dataset:
+        memory_format=torch.channels_last
+        base_folder = '/home/sankeerth/ev/data/Caltech_data/'
+        dataset = ContinuousEventsDataset(base_folder=base_folder, event_folder='events/data',\
+                width=width, height=height, evframe_type='histogram', window_size = 0.05, time_shift = 0.001) # 1 ms time shift
+    else:
+        dataset_path = '/home/sankeerth/ev/rpg_event_representation_learning/N-Caltech101/testing/'
+        dataset = NCaltech101(
+            dataset_path, 
+            'all', 
+            height, 
+            width, 
+            25000, 
+            mode='validation', 
+            event_representation='histogram', 
+            shuffle=False
+        )
 
-
-    flags = FLAGS()
-    test_dataset = NCaltech101('/home/sankeerth/ev/rpg_event_representation_learning/N-Caltech101/testing/')
-    test_loader = Loader(test_dataset, flags, device)
+    test_loader = DataLoader(dataset)
 
 
     model = ClassifierIncrEval()
@@ -62,17 +51,30 @@ if __name__ == '__main__':
     sum_accuracy = 0
     sum_loss = 0
 
-    for events, labels in tqdm(test_loader):
-        with torch.no_grad():
-            pred_labels, _ = model.forward_refresh_reservoir(events)
-            loss, accuracy = cross_entropy_loss_and_accuracy(pred_labels, labels)
-        sum_accuracy += accuracy
-        sum_loss += loss
 
-    test_loss = sum_loss.item() / len(test_loader)
-    test_accuracy = sum_accuracy.item() / len(test_loader)
+    with profile(activities=[ProfilerActivity.CUDA, ProfilerActivity.CPU], with_stack=True) as prof:
 
-    print(f"Test Loss: {test_loss}, Test Accuracy: {test_accuracy}")
+        for i_batch, sample in tqdm(test_loader):
+            events, labels = sample
+            with torch.no_grad():
+                if i_batch%1 == 0:
+                    with record_function("model_inference_base"):
+                        pred_labels, _ = model.forward_refresh_reservoir(events)
+        
+                else:
+                    with record_function("model_inference_base"):
+                        out, mask = model(events)
+                        model_out += out
+        
+                    # loss, accuracy = cross_entropy_loss_and_accuracy(pred_labels, labels)
+
+
+            # sum_accuracy += accuracy
+            # sum_loss += loss
+
+    # test_loss = sum_loss.item() / len(test_loader)
+    # test_accuracy = sum_accuracy.item() / len(test_loader)
+    # print(f"Test Loss: {test_loss}, Test Accuracy: {test_accuracy}")
 
         # show_tensor_image()
 
--- run_objdet_incr.py
+++ run_objdet_incr.py
@@ -1,7 +1,10 @@
+from os import listdir
+import os
 import torch
 import torch.nn.functional as F
 import numpy as np
 from torch.utils.data import DataLoader
+from data_fetchers.continuous_event_datasets import ContinuousEventsDataset
 # import Caltech101ContinuousEvDataset
 from incr_modules.obj_detection_incr import DenseObjectDetIncr
 from ev_projs.rpg_async.models.yolo_loss import yoloLoss
@@ -13,35 +16,35 @@ from torch.profiler import profile, record_function, ProfilerActivity
 if __name__ == '__main__':
 
     device='cuda'
-    # memory_format=torch.channels_last
-    # base_folder = '/home/sankeerth/ev/depth_proj/data/test/'
-
-    # dataset = ContinuousEventsDataset(base_folder=base_folder, event_folder='events/data',\
-    #         width=346, height=260, window_size = 0.05, time_shift = 0.05) # 1 ms time shift
-
     height, width = 180, 240
-    dataset_path = '/home/sankeerth/ev/rpg_asynet/data/NCaltech101_ObjectDetection/'
-    dataset = NCaltech101_ObjectDetection(
-        dataset_path, 
-        'all', 
-        height, 
-        width, 
-        25000, 
-        mode='validation', 
-        event_representation='histogram', 
-        shuffle=False
-    )
+    continuous_dataset = False
+    if continuous_dataset:
+        memory_format=torch.channels_last
+        base_folder = '/home/sankeerth/ev/data/Caltech_data/'
+        dataset = ContinuousEventsDataset(base_folder=base_folder, event_folder='events/data',\
+                width=width, height=height, evframe_type='histogram', window_size = 0.05, time_shift = 0.001) # 1 ms time shift
+    else:
+        dataset_path = '/home/sankeerth/ev/rpg_asynet/data/NCaltech101_ObjectDetection/'
+        dataset = NCaltech101_ObjectDetection(
+            dataset_path, 
+            'all', 
+            height, 
+            width, 
+            25000, 
+            mode='validation', 
+            event_representation='histogram', 
+            shuffle=False
+        )
 
-    model = DenseObjectDetIncr(dataset.nr_classes, in_c=2,
-                                    small_out_map=(True))
+    val_loader = DataLoader(dataset, shuffle=False, batch_size=1)
+
+    
+    model = DenseObjectDetIncr(101, in_c=2, small_out_map=True)
     lp = '/home/sankeerth/ev/rpg_asynet/log/20220508-215001/checkpoints/model_step_49.pth'
     m = torch.load(lp)
     model.load_state_dict(m['state_dict'])
     model = model.to(device)
     model.eval()
-
-    val_loader = DataLoader(dataset, shuffle=False, batch_size=1)
-
     model_input_size = torch.tensor([191, 255])
     sum_accuracy = 0
     sum_loss = 0
@@ -49,10 +52,6 @@ if __name__ == '__main__':
     with profile(activities=[ProfilerActivity.CUDA, ProfilerActivity.CPU], with_stack=True) as prof:
 
         for i_batch, sample_batched in enumerate(val_loader):
-
-            if i_batch == 20:
-                break
-
 
             event, bounding_box, histogram = sample_batched
 
@@ -88,7 +87,12 @@ if __name__ == '__main__':
             # detected_bbox = nonMaxSuppression(detected_bbox, iou=0.6)
             # detected_bbox = detected_bbox.cpu().numpy()
 
-        # sum_loss += loss
+            # sum_loss += loss
+
+            if i_batch == 20:
+                break
+
+
 
     print(prof.key_averages().table(sort_by="{}_time_total".format(device), row_limit=30))
 
