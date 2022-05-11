baf7d461d843a6cc9749726713e65e726d84a414
sankeerth <sankeerth95@gmail.com>
2022-05-10 21:13:25 -0400

    e2depth working and profiling now

--- run_e2depth_continuous_dataset.py
+++ run_e2depth_continuous_dataset.py
@@ -1,6 +1,8 @@
 import argparse
+import profile
 import torch
 from torch.nn import ReflectionPad2d
+from torch.profiler import profile, record_function, ProfilerActivity
 
 from data_fetchers.continuous_event_datasets import ContinuousEventsDataset
 from data_fetchers.voxel_event_dataset import VoxelGridDataset
@@ -13,14 +15,16 @@ from ev_projs.rpg_e2depth.options.inference_options import set_depth_inference_o
 if __name__ == '__main__':
     device='cuda'
     visualize=False
-
+    continuous_data = True
 
     width, height = 346, 260
     base_folder = '/home/sankeerth/ev/depth_proj/data/test/'
     # memory_format
-    # dataset = ContinuousEventsDataset(base_folder=base_folder, event_folder='events/data',\
-    #         width=width, height=height, window_size = 0.05, time_shift = 0.05) # 1 ms time shift
-    dataset = VoxelGridDataset(base_folder, 'events/voxels', transform=None, normalize=False)
+    if continuous_data:
+        dataset = ContinuousEventsDataset(base_folder=base_folder, event_folder='events/data',\
+                width=width, height=height, window_size = 0.05, time_shift = 0.05) # 1 ms time shift
+    else:
+        dataset = VoxelGridDataset(base_folder, 'events/voxels', transform=None, normalize=False)
 
     pth = '/home/sankeerth/ev/experiments/pretrained_models/E2DEPTH_si_grad_loss_mixed.pth.tar'    
     model = load_model_incr(pth).to(device).eval()
@@ -48,19 +52,23 @@ if __name__ == '__main__':
             pad(event_preprocessor(torch.Tensor(dataset[i]))),
             dim=0).to(device)#, memory_format=memory_format)
 
-        for i in range(5):
-            event_tensor = get_data_i(i)
 
-            if i % 20 == 0:
-                c,h = model.forward_refresh_reservoirs(event_tensor, None)
-                event_tensor_prev = event_tensor
-            else:
-                c_incr, h_incr = model((event_tensor - event_tensor_prev, None), None)
-                event_tensor_prev = event_tensor
-                c += c_incr[0]
+        with profile(activities=[ProfilerActivity.CUDA, ProfilerActivity.CPU], with_stack=True) as prof:
 
-        print(c)
+            with torch.no_grad():
+                for i in range(20):
+                    event_tensor = get_data_i(i)
 
-
-
+                    if i % 20 == 0:
+                        with record_function("model_inference_base"):
+                            c,h = model.forward_refresh_reservoirs(event_tensor, None)
+                        event_tensor_prev = event_tensor
+                    else:
+                        x = event_tensor - event_tensor_prev
+                        with record_function("model_inference"):
+                            c_incr, h_incr = model((x, None), None)
+                        event_tensor_prev = event_tensor
+                        c += c_incr[0]
+    
+        print(prof.key_averages().table(sort_by="{}_time_total".format(device), row_limit=15))
 
